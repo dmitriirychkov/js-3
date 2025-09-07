54f2ffeeb92b671a14228d2adb748c85eeaab43e
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-09-07 21:35:44 +0500

    Add files via upload

---
+++ lesson-13-01/index.js
@@ -0,0 +1,46 @@
+// Исходная функция-конструктор
+/*
+function Student(name, age) {
+    this.name = name;
+    this.age = age;
+    this.technologies = [];
+    this.status = 'Junior';
+
+    this.setTechnologies = function(technologies) {
+        this.technologies = [
+            ...this.technologies,
+            ...technologies,
+        ];
+    }
+
+    this.setNewStatus = function(newStatus) {
+        this.status = newStatus;
+    }
+}
+*/
+// Преобразованный класс
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.technologies = [];
+        this.status = 'Junior';
+    }
+
+    setTechnologies(technologies) {
+        this.technologies = [
+            ...this.technologies,
+            ...technologies,
+        ];
+    }
+
+    setNewStatus(newStatus) {
+        this.status = newStatus;
+    }
+}
+
+// Пример использования
+const student = new Student('Maxim', 20);
+student.setTechnologies(['HTML', 'CSS', 'JavaScript']);
+student.setNewStatus('Middle');
+console.log(student);
